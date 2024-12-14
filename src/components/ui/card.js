2ea2abf60e3804ba30369918560534276050b401
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-12-14 16:55:36 +0900

    fix card h3 error

--- src/components/ui/card.js
+++ src/components/ui/card.js
@@ -22,16 +22,18 @@ const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
 ))
 CardHeader.displayName = "CardHeader"
 
-const CardTitle = React.forwardRef(({ className, ...props }, ref) => (
-  <h3
-    ref={ref}
-    className={cn(
-      "text-2xl font-semibold leading-none tracking-tight",
-      className
-    )}
-    {...props}
-  />
-))
+const CardTitle = React.forwardRef(({ children, className, ...props }, ref) => (
+    <h3
+      ref={ref}
+      className={cn(
+        "text-2xl font-semibold leading-none tracking-tight",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </h3>
+  ))
 CardTitle.displayName = "CardTitle"
 
 const CardContent = React.forwardRef(({ className, ...props }, ref) => (
